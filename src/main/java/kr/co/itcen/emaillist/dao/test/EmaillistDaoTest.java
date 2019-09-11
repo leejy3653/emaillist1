e8addc92f74bdbdde31a56651684b5096be80e70
ljy <leejy3653@naver.com>
2019-09-11 13:10:33 +0900

    init commit

---
+++ src/main/java/kr/co/itcen/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,37 @@
+package kr.co.itcen.emaillist.dao.test;
+
+import java.util.List;
+
+import kr.co.itcen.emaillist.dao.EmaillistDao;
+import kr.co.itcen.emaillist.vo.EmaillistVo;
+
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		insertTest();
+		getListTest();
+		
+		deleteAll();
+	}
+	
+	private static void deleteAll() {
+		new EmaillistDao().delete();
+	}
+	
+	private static void insertTest() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("종윤");
+		vo.setLastName("이");
+		vo.setEmail("leejy3653@gmail.com");		
+		new EmaillistDao().insert(vo);
+	}
+
+	private static void getListTest() {
+		List<EmaillistVo> list = new EmaillistDao().getList();
+		for(EmaillistVo vo : list) {
+			System.out.println(vo);
+		}
+	}
+
+}
